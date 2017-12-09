ad5c627cb9e4175d610f74539356291b9df61197
Josh Green <joshuagreen118@gmail.com>
2017-12-09 11:46:39 -0500

    Added IOTA, Added contact, removed Paypal
    Replaced paypal dontaion with IOTA dontaion. Added conatct form to get feedback from users!

--- pci-web/main.go
+++ pci-web/main.go
@@ -6,9 +6,9 @@ import(
 )
 
 // compile all templates and cache them
-var templates = template.Must(template.ParseGlob("/home/josh/go/src/pci-web/templates/*"))
+var templates = template.Must(template.ParseGlob("templates/*"))//home/josh/go/src/pci-web/
 func main(){
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("/home/josh/go/src/pci-web/css/"))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css/"))))//home/josh/go/src/pci-web/
     http.HandleFunc("/contribute.json", contribute)
 	http.HandleFunc("/", IndexHandler)
 	http.ListenAndServe(":8001", nil)
@@ -19,7 +19,20 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Security-Policy:", "default-src https:")
 
     action := r.URL.Query().Get("action")
-    if len(action) == 0 {
+    if len(action) == 0{
+        err := templates.ExecuteTemplate(w, "indexPage", nil)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+    } else if action == "message" {
+        //Send email then
+        name := r.URL.Query().Get("name")
+        email := r.URL.Query().Get("email")
+        message := r.URL.Query().Get("message")
+
+        mail(name, email, message)
+
         err := templates.ExecuteTemplate(w, "indexPage", nil)
         if err != nil {
             http.Error(w, err.Error(), http.StatusInternalServerError)

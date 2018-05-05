97a8d519d911ddb60fc2d1d1a02cbadb602c8bf9
Josh Green <joshuagreen118@gmail.com>
2018-05-05 14:14:33 -0400

    Added mail functionality
    Forgot to incorporate contact for in first push. Here it is!

--- pci-web/index.php
+++ pci-web/index.php
@@ -4,7 +4,21 @@ require_once('header.php');
 
 if(isset($_GET['action']) && $_GET['action'] == "submit"){
 	require_once('resultsTable.php');
-} else if(isset($_GET['action']) && $_GET['action'] == "update"){
+} else if(isset($_GET['action']) && $_GET['action'] == "messsage"){
+
+	$to = 'admin@pcilookup.com';
+	$subject = 'PCI Lookpup Contact Form';
+	$msg = $_GET['message'];
+	$headers  = "From: " . $_GET['name'] . " < " . $_GET['email'] . " >\n";
+    $headers .= "Cc: testsite < " . $_GET['email'] . " >\n"; 
+    $headers .= "X-Sender: testsite < " . $_GET['email'] . " >\n";
+    $headers .= 'X-Mailer: PHP/' . phpversion();
+    $headers .= "X-Priority: 1\n"; // Urgent message!
+    $headers .= "Return-Path: " . $_GET['email'] . "\n"; // Return path for errors
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=iso-8859-1\n";
+
+	mail($to, $subject, $msg, $headers);
 
 } else {
 	require_once('form.php');

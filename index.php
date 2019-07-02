d53900d9d2253d65de4bb83ff67896c89af734b5
Josh Green <joshuagreen118@gmail.com>
2019-07-02 10:54:45 -0400

    Fixed contact form

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if(isset($_GET['action']) && $_GET['action'] == "submit"){
     // Build POST request:
     $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
     $recaptcha_secret = '6Ldbm6sUAAAAAJnxNGYYGxFReAlcS_seZGAZBRpd';
-    $recaptcha_response = $_POST['recaptcha_response'];
+    $recaptcha_response = $_GET['recaptcha_response'];
 
     // Make and decode POST request:
     $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);

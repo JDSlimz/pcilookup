ad55aa74414cb24bc651e2cfd4cd87846153292d
Josh Green <josh@pcilookup.com>
2020-07-03 10:56:52 -0400

    Removed Matomo code, added vultr banner.

--- foot.php
+++ foot.php
@@ -9,36 +9,15 @@
 				<img class="coinLogo" src="/img/btclogo.png" data-toggle="modal" data-target="#btcModal" />
 				<img class="coinLogo" src="/img/ltclogo.png" data-toggle="modal" data-target="#ltcModal" />
 				<img class="coinLogo" src="/img/nanologo.png" data-toggle="modal" data-target="#nanoModal" />
-				
-				
+			</div>
 
-			<!-- <div id="support-privacy">
-				<i>Adblockers are great for privacy! So are cryptocurrencies! We&nbsp;support privacy!</i>
-			</div> -->
+			<h5>This site is hosted on Vultr. You can help keep it running and get some free VPS time at the same time!</h5>
+			<a href="https://www.vultr.com/?ref=8374849-6G"><img src="https://www.vultr.com/media/banners/banner_468x60.png" width="468" height="60"></a>
 
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 			<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script>
 			<script src="/js/bootstrap.min.js"></script>
 			<script src="/js/script.js"></script>
-
-<!-- Matomo -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//piwik.pcilookup.com/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Matomo Code -->
-
-
-
 		</div>
 	</body>
 </html>

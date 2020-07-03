0c373a0b7c915fde4114b512fab9d94c7f60fe5a
Josh Green <josh@pcilookup.com>
2020-07-03 11:00:21 -0400

    Moved vultr banner to ourside footer.

--- foot.php
+++ foot.php
@@ -10,17 +10,16 @@
 				<img class="coinLogo" src="/img/ltclogo.png" data-toggle="modal" data-target="#ltcModal" />
 				<img class="coinLogo" src="/img/nanologo.png" data-toggle="modal" data-target="#nanoModal" />
 			</div>
-			<br>
-			<div id="vultrBanner">
-				<small>This site is hosted on Vultr. You can help keep it running and get some free VPS time at the same time!</small>
-				<br>
-				<a href="https://www.vultr.com/?ref=8374849-6G"><img src="https://www.vultr.com/media/banners/banner_468x60.png" width="468" height="60"></a>
-			</div>
 
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 			<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script>
 			<script src="/js/bootstrap.min.js"></script>
 			<script src="/js/script.js"></script>
+		</div>
+		<div id="vultrBanner">
+			<small>This site is hosted on Vultr. You can help keep it running and get some free VPS time at the same time!</small>
+			<br>
+			<a href="https://www.vultr.com/?ref=8374849-6G"><img src="https://www.vultr.com/media/banners/banner_468x60.png" width="468" height="60"></a>
 		</div>
 	</body>
 </html>
